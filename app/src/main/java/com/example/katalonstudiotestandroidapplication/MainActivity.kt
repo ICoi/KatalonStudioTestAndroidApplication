620b3f35e9d7c7b3634c858ecb2c5ab2b709a557
DaunJoung <ico1022@gmail.com>
2021-04-20 20:33:38 +0900

    Implement sample

--- app/src/main/java/com/example/katalonstudiotestandroidapplication/MainActivity.kt
+++ app/src/main/java/com/example/katalonstudiotestandroidapplication/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.katalonstudiotestandroidapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val editText = findViewById<EditText>(R.id.nameTF)
+        editText.addTextChangedListener( object : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.i("Test", "$p0")
+                val textView = findViewById<TextView>(R.id.helloWorldTV).apply {
+                    text = "Hello $p0"
+                }
+            }
+        })
+
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.nameTF)
+        val message = editText.text.toString()
+        Log.i("Test", "Input name : $message")
+        val intent = Intent(this, SubActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
